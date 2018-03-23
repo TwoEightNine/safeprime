f02bc752213d402efb2cff679b35b70e7bad9587
Vladislav <butorovva@altarix.ru>
2018-03-24 01:07:51 +0400

    Initial

---
+++ dhprime.py
@@ -0,0 +1,22 @@
+import os
+from base64 import b64decode as b64
+
+HEADER = '-----BEGIN DH PARAMETERS-----\n'
+FOOTER = '\n-----END DH PARAMETERS-----'
+
+BITS = 2048
+PEM_FILE = "dh.pem"
+PRIME_FILE = "safePrime"
+
+os.system("openssl dhparam -out %s -2 %d" % (PEM_FILE, BITS))
+
+s = ""
+with open(PEM_FILE, 'r') as f:
+    s = f.read()
+
+cert = s[s.find(HEADER) + len(HEADER):s.find(FOOTER)]
+prime = b64(cert)[7:-3]
+hx = ''.join([hex(ord(i))[2:].zfill(2) for i in prime])
+
+with open(PRIME_FILE, 'w') as f:
+    f.write(str(int(hx, 16)))
